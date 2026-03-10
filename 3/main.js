af4a93eb678ecbc4b5bfcd15e94a29e91445d42d
Impluse2 <golusrkaiiii06@gmail.com>
2026-03-10 14:52:49 +0300

    Add files via upload

---
+++ 3/main.js
@@ -0,0 +1,17 @@
+// Задача 1
+const firstName = "Ivan";
+const lastName = "Golushko";
+const isStudent = true;
+
+// Задача 2
+const age = 20;
+const currentYear = 2026;
+const birthYear = currentYear - age;
+
+// Задача 3
+console.log(
+  `Меня зовут: ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`,
+);
+
+// Задача 4
+// 123456789falsetrue
