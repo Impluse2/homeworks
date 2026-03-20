f2e441590ecdff0492b630abe506e13a6ac41229
Impluse2 <golusrkaiiii06@gmail.com>
2026-03-20 14:04:51 +0300

    Add files via upload

---
+++ todo(DOM)/js/main.js
@@ -0,0 +1,89 @@
+"use strict";
+
+const todoKeys = {
+  id: "id",
+  text: "text",
+  is_completed: "is_completed",
+};
+
+// const todos = [];
+
+const errTodoNotFound = (todoId) => `Todo with id ${todoId} not found`;
+
+const getNewTodoId = (todos) =>
+  todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+
+const createTodo = (todos, text) => {
+  const newTodo = {
+    [todoKeys.id]: getNewTodoId(todos),
+    [todoKeys.text]: text,
+    [todoKeys.is_completed]: false,
+  };
+  todos.push(newTodo);
+  return newTodo;
+};
+
+const completeTodoById = (todos, todoId) => {
+  const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+
+  if (!todo) {
+    console.error(errTodoNotFound(todoId));
+    return null;
+  }
+  todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+  return todo;
+};
+
+const deleteTodoById = (todos, todoId) => {
+  const todoIndex = todos.findIndex((todo) => todo[todoKeys.id] === todoId);
+  if (todoIndex === -1) {
+    console.error(errTodoNotFound(todoId));
+    return todos;
+  }
+  todos.splice(todoIndex, 1);
+  return todos;
+};
+
+// При помощи метода querySelector получаем элементы .form, .input и .todos
+// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
+// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const todos = document.querySelector(".todos");
+
+function createTodoElement(text) {
+  const li = document.createElement("li");
+  li.className = "todo";
+
+  li.innerHTML = `
+    <div class="todo-text">${text}</div>
+    <div class="todo-actions">
+      <button class="button-complete button">&#10004;</button>
+      <button class="button-delete button">&#10006;</button>
+    </div>
+  `;
+
+  const completeButton = li.querySelector(".button-complete");
+  completeButton.onclick = () => {
+    li.classList.toggle("completed");
+  };
+
+  const deleteButton = li.querySelector(".button-delete");
+  deleteButton.onclick = () => li.remove();
+
+  return li;
+}
+
+function handleCreateTodo(todos, text) {
+  if (text.trim() === "") return;
+
+  const TodoElement = createTodoElement(text);
+  todos.appendChild(TodoElement);
+  input.value = "";
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  handleCreateTodo(todos, input.value);
+});
